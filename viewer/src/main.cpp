7d8a098bf68fc123da224e25db6def039645db39
geb0598 <89014972+geb0598@users.noreply.github.com>
2026-03-27 01:26:32 +0900

    chore: set app icon and window title

--- viewer/src/main.cpp
+++ viewer/src/main.cpp
@@ -10,20 +10,22 @@ LRESULT WINAPI WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     if (g_viewer) {
         return g_viewer->HandleMessage(hwnd, msg, wp, lp);
     }
-    return DefWindowProc(hwnd, msg, wp, lp);
+    return DefWindowProcW(hwnd, msg, wp, lp);
 }
 
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE, LPSTR, int) {
     WNDCLASSEXW wc        = {};
-    wc.cbSize            = sizeof(WNDCLASSEX);
+    wc.cbSize            = sizeof(WNDCLASSEXW);
     wc.style             = CS_CLASSDC;
     wc.lpfnWndProc       = WndProc;
     wc.hInstance         = hinstance;
+    wc.hIcon             = LoadIcon(hinstance, MAKEINTRESOURCE(101));
+    wc.hIconSm           = LoadIcon(hinstance, MAKEINTRESOURCE(101));
     wc.lpszClassName     = L"cpp-insights-viewer";
     RegisterClassExW(&wc);
 
     HWND hwnd = CreateWindowW(
-        wc.lpszClassName, L"cpp-insights",
+        wc.lpszClassName, L"cpp-insights-viewer",
         WS_OVERLAPPEDWINDOW,
         100, 100, 1280, 960,
         nullptr, nullptr,
